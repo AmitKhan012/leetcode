420e573cd58efe239a375a43ecfd27909b8708f5
AmitKhan012 <uchihaamit1@gmail.com>
2023-02-12 10:49:28 +0600

    Create 1911.py

---
+++ 1911.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        sumEven, sumOdd = 0, 0
+
+        for i in range(len(nums) - 1, -1, -1):
+            tmpEven = max(sumOdd + nums[i], sumEven)
+            tmpOdd = max(sumEven - nums[i], sumOdd)
+            sumEven, sumOdd = tmpEven, tmpOdd
+        return sumEven
